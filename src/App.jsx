436b9ed5aca0bffae45b50e478bd11bb7a2e1f8a
smshah121 <smshah.2003@gmail.com>
2025-08-23 20:54:30 +0500

    changes saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import React, { useState } from 'react'
-import { IoMdSearch } from "react-icons/io";
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { WiHumidity } from "react-icons/wi";
-import { FaWind } from "react-icons/fa";
-import axios from 'axios';
-
-
-const App = () => {
-  const [search, setSearch] = useState("")
-  const [loading, setLoading] = useState(false)
+import { WiStrongWind } from "react-icons/wi";
+import axios from "axios";
+function App() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
   const [temperature, setTemperature] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [windSpeed, setWindSpeed] = useState(null);
   const [cityName, setCityName] = useState("");
   const [weatherIcon, setWeatherIcon] = useState("01d");
+
   const API_KEY = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
-  const fetchWeather = async ()=>{
+
+  const fetchWeather = async () => {
+    console.log(search);
+    if (!search) return;
+    setLoading(true);
     try {
-      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${API_KEY}`)
-      if(data.cod){
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${API_KEY}`
+      );
+      console.log(data);
+      if (data.cod === 200) {
         setTemperature(data.main.temp);
         setHumidity(data.main.humidity);
         setWindSpeed(data.wind.speed);
         setCityName(data.name);
         setWeatherIcon(data.weather[0].icon);
-
       }
-
     } catch (error) {
       console.log(error);
       setCityName("city not found");
@@ -33,51 +37,67 @@ const App = () => {
       setHumidity(null);
       setWindSpeed(null);
       setWeatherIcon("01d");
-
-      
     }
     setLoading(false);
-  }
+  };
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-br from-purple-800 to-slate-700 text-white'>
-      <div className='flex items-center bg-white rounded-full px-3 py-2 w-50 mb-5 shadow-lg'>
-        <input type="text"
-        placeholder='Search' 
-        value={search}
-        onChange={(e)=> setSearch(e.target.value)}
-        className='flex-1 text-black outline-none px-2'/>
-        <IoMdSearch
-       onClick={fetchWeather}
-        className='text-gray-700 cursor-pointer'/>
-      </div>
-      <img src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="" className='w-20 h-20 mb-2'/>
-      <h1 className='text-4xl font-bold mb-2'>
-        {loading 
-        ? "loading..."
-        :temperature!=null
-        ?`${temperature}°C`
-        :"__"}</h1>
-      <h2 className='text-2xl font-semibold'>
-        {cityName || "Enter a City Name"}
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-purple-950 to-blue-600 text-white">
+        <h1 className="my-12 text-2xl text-gray-200 font-bold">Weather App</h1>
+        {/* search bar & icon */}
+        <div className="flex items-center bg-white rounded-full px-4 py-2 mb-6 w-80 shadow-lg">
+          <input
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="flex-1 text-black outline-none px-2"
+          />
+          <FaSearch
+            onClick={fetchWeather}
+            className="text-gray-500 cursor-pointer"
+          />
+        </div>
+
+        {/* weather icon */}
+        <img
+          src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+          alt=""
+          className="w-20 h-20 mb-4"
+        />
+
+        {/* Temprature & CityName */}
+        <h1 className="text-4xl font-bold mb-1">
+          {loading
+            ? "loading..."
+            : temperature !== null
+            ? `${temperature}°C`
+            : "--"}
+        </h1>
+        <h2 className="text-2xl mt-2 font-semibold">
+          {cityName || "type to check temperature"}
         </h2>
-      <div className='w-80 flex items-center justify-between mt-5'>
-        <div className='flex flex-col items-center'>
-          <WiHumidity className='text-2xl mb-1'/>
-          <span className='text-lg'>
-          {humidity !== null ? `${humidity}%` : "--"}
-          </span>
-          <p className='text-sm'>Humidity</p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <FaWind className='text-2xl mb-1'/>
-          <span className='text-lg'>
-          {windSpeed !== null ? `${windSpeed}km/h` : "--"}
-          </span>
-          <p className='text-sm'>wind speed</p>
+
+        {/* Humadity & Wind Speed */}
+        <div className="w-full max-w-md mt-7 flex flex-col md:flex-row items-center justify-between md:items-start">
+          <div className="flex flex-col items-center ">
+            <WiHumidity className="text-3xl" />
+            <span className="text-lg font-medium">
+              {humidity !== null ? `${humidity}%` : "--"}
+            </span>
+            <p className="text-sm">Humidity</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <WiStrongWind className="text-3xl" />
+            <span className="text-lg font-medium">
+              {windSpeed !== null ? `${windSpeed}km/h` : "--"}
+            </span>
+            <p className="text-sm">Wind Speed</p>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_OPENWEATHERMAP_API_KEY': JSON.stringify(process.env.VITE_OPENWEATHERMAP_API_KEY)
-  }
+  
 })
